5a7dad97eb2f3520a254bb9980153a575a96ad97
theDashpuntsag <thedashpuntsag@gmail.com>
2023-03-08 21:21:26 +0800

    hello World

--- valid-parentheses/ValidParentheses.java
+++ valid-parentheses/ValidParentheses.java
@@ -2,11 +2,8 @@ import java.util.Stack;
 
 public class ValidParentheses {
     public boolean isValid(String s) {
-
-
         // Create a new stack to store the characters.
         Stack<Character> stack = new Stack<>();
-
 
         // convert string into char array and access the characters using for each loop.
         for(char ch: s.toCharArray()) {
